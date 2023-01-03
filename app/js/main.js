375d1a6bf2dfaf51e263afcd2c0b9ca1b651b278
Vadim <weasfear@mail.ru>
2023-01-03 22:40:59 +0300

    update : adaptive design

--- app/js/main.js
+++ app/js/main.js
@@ -34,6 +34,50 @@ $('.contact-slider').slick({
     slidesToScroll: 10, //перемещать сразу по 10
     dots: true,
     arrows: false,
+    responsive: [
+    {
+      breakpoint: 1700,
+      settings: {
+        slidesToShow: 8,
+        slidesToScroll: 8,
+      }
+    },
+    {
+      breakpoint: 1511,
+      settings: {
+        slidesToShow: 6,
+        slidesToScroll: 6,
+      }
+    },
+    {
+      breakpoint: 1201,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4,
+      }
+    },
+    {
+      breakpoint: 841,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 551,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 376,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+    ]
 })
 
 
